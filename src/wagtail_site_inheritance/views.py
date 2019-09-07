b42296a006b0a601750b88acc1c5855e1e8ef1f2
Mike Dingjan <m.dingjan@labdigital.nl>
2019-09-07 11:57:25 +0200

    Remove all PageInheritanceItems on SiteInheritance removal
    This makes sure the site gets detached from the "previous" parent site.

---
+++ src/wagtail_site_inheritance/views.py
@@ -0,0 +1,19 @@
+from django.db import transaction
+from wagtail.contrib.modeladmin.views import DeleteView
+from wagtail.core.models import Page
+
+from wagtail_site_inheritance.models import PageInheritanceItem
+
+
+class SiteInheritanceDeleteView(DeleteView):
+    @transaction.atomic()
+    def post(self, request, *args, **kwargs):
+        """Remove the PageInheritanceItems before removing the SiteInheritance.
+
+        This way we ensure that the site will be detached from further syncing and
+        prevent a lot of unexpected behavior.
+
+        """
+        pages_in_site = Page.objects.in_site(self.instance.site).values("pk")
+        PageInheritanceItem.objects.filter(inherited_page_id__in=pages_in_site).delete()
+        return super().post(request, *args, **kwargs)
