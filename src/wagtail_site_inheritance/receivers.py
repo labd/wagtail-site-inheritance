79fcdcb55bd16350420217ee400d65c00209eed0
Mike Dingjan <m.dingjan@labdigital.nl>
2019-09-09 10:20:11 +0200

    Implement page_unpublish receiver to sync inherited sites

---
+++ src/wagtail_site_inheritance/receivers.py
@@ -0,0 +1,26 @@
+from django.db import transaction
+from wagtail.core.signals import page_unpublished
+from wagtail_site_inheritance.models import PageInheritanceItem
+
+
+def remove_copies(sender, instance, **kwargs):
+    """Remove all published copies when un publishing a page.
+
+    For now we remove all non modified copies of the inherited page on unpublish for the
+    modified copies we only remove the page link and make them "stand-alone".
+
+    """
+    items = PageInheritanceItem.objects.filter(page=instance)
+    if not items.exists():
+        return
+
+    with transaction.atomic():
+        for item in items:
+            if not item.modified:
+                item.inherited_page.delete()
+
+            item.delete()
+
+
+def register_handlers():
+    page_unpublished.connect(remove_copies)
