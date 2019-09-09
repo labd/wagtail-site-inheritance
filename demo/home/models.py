62cabe1d6631b229920db51b823fab975a359447
David Weterings <david@davidweterings.nl>
2019-09-09 15:28:10 +0200

    Specify which fields can be overwritten in inherited pages, copy m2m page fields

--- demo/home/models.py
+++ demo/home/models.py
@@ -14,3 +14,10 @@ class OtherPage(PageInheritanceMixin, Page):
     custom_content = models.CharField(max_length=500, default="")
 
     content_panels = Page.content_panels + [FieldPanel("intro"), FieldPanel("custom_content")]
+
+    @property
+    def customizable_fields(self):
+        """
+        Fields that can be overwritten in other sites.
+        """
+        return ["custom_content"]
