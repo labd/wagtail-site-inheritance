e5aef6adb2d265ab91ad4b826fc8dff18341ae3f
Mike de Bock <mike.debock@masterwebber.nl>
2020-01-13 20:54:05 +0100

    added search on page_id to django-admin and use raw_id_fields

--- src/wagtail_site_inheritance/admin.py
+++ src/wagtail_site_inheritance/admin.py
@@ -7,9 +7,12 @@ from wagtail_site_inheritance import models
 class PageInheritanceItemAdmin(admin.ModelAdmin):
     list_display = ["page", "inherited_page", "modified"]
     list_filter = ["modified"]
+    raw_id_fields = ["page", "inherited_page"]
+    search_fields = ["=page__id", "=inherited_page__id"]
 
 
 @admin.register(models.SiteInheritance)
 class SiteInheritanceAdmin(admin.ModelAdmin):
     list_display = ["parent", "site"]
     list_filter = ["parent"]
+    raw_id_fields = ["parent", "site"]
