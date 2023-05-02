5edab2b673a78ef19ddca0ddf61be76cb0e6fdae
a.aalberts <a.aalberts@labdigital.nl>
2023-05-02 15:15:37 +0200

    Bump version: 0.0.1 → 1.0.0

--- src/wagtail_site_inheritance/__init__.py
+++ src/wagtail_site_inheritance/__init__.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 
 
 class WagtailSiteInheritanceAppConfig(AppConfig):
